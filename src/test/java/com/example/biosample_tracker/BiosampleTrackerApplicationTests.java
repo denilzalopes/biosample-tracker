34028ec9e31c4127cdad211e78769007ffd5c06f
Denilza <denylzalopes@gmail.com>
2025-07-18 01:16:49 +0200

    Initial commit: Spring Boot project setup

---
+++ src/test/java/com/example/biosample_tracker/BiosampleTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.biosample_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiosampleTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
